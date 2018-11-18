b1ddb67ec0ed46586f5a89b22ac287a381e272e9
carolreis <caroline.s_reis@icloud.com>
2018-11-18 00:38:29 -0200

    Adição de novos parametros

--- index.php
+++ index.php
@@ -29,20 +29,35 @@ foreach ($lista_itens as $item) {
 /* Get POST data */
 $data = json_decode(file_get_contents('php://input'));
 
-$limite = isset($data->limite_espaco) ? $data->limite_espaco : 7000;
 $tamanho_populacao = isset($data->tamanho_populacao) ? $data->tamanho_populacao : 20;
-$taxa_mutacao = isset($data->taxa_mutacao) ? $data->taxa_mutacao : 0.01;
 $numero_geracoes = isset($data->quantidade_geracoes) ? $data->quantidade_geracoes : 100;
+$numero_pais = isset($data->quantidade_pais) ? $data->quantidade_pais: 2;
+$numero_individuos = isset($data->numero_individuos) ? $data->numero_individuos: 2; // novos individuos
+$taxa_mutacao = isset($data->taxa_mutacao) ? $data->taxa_mutacao : 0.01;
+$bits_mutacao = isset($data->bits_mutacao) ? $data->bits_mutacao: 5;
+$peso_maximo = isset($data->peso_maximo) ? $data->peso_maximo: 7000;
+$peso_maximo_obj = isset($data->peso_maximo_obj) ? $data->peso_maximo_obj: 7000;
+$peso_min_obj = isset($data->peso_min_obj) ? $data->peso_min_obj: 50;
+$valor_maximo_obj = isset($data->valor_maximo_obj) ? $data->valor_maximo_obj: 7000;
+$valor_min_obj = isset($data->valor_min_obj) ? $data->valor_min_obj: 50;
+
 
 /* Debugging purpose */
-echo "\nLimite: ".$limite;
-echo "\nTamanho população: ".$tamanho_populacao;
-echo "\ntaxa_mutacao: ".$taxa_mutacao;
-echo "\nQuantidade Gerações: ".$numero_geracoes;
+echo "\nTamanho População: ".$tamanho_populacao;
+echo "\nNúmero Gerações: ".$numero_geracoes;
+echo "\nNúmero Pais: ".$numero_pais;
+echo "\nNúmero de Indivíduos: ".$numero_individuos;
+echo "\nTaxa_de Mutacao: ".$taxa_mutacao;
+echo "\nBits Mutacao: ".$bits_mutacao;
+echo "\nPeso Máximo: ".$peso_maximo;
+echo "\nPeso Máximo Objeto: ".$peso_maximo_obj;
+echo "\nPeso Mínimo Objeto: ".$peso_min_obj_obj;
+echo "\nValor Máximo Objeto: ".$valor_maximo_obj;
+echo "\nValor Mínimo Objeto: ".$valor_min_obj_obj;
 
 $ag = new AlgoritmoGenetico($tamanho_populacao);
 
-$resultado = $ag->resolver($taxa_mutacao, $numero_geracoes, $espacos, $valores, $limite);
+$resultado = $ag->resolver($taxa_mutacao, $numero_geracoes, $espacos, $valores, $peso_maximo);
 
 http_response_code(200);
 echo "\nCromossomo: ";
